80923bd45b944f6677f138742be06200e8faf988
Konstantin Pavlov <konstantin.pavlov@payconiq.com>
2019-03-07 05:21:43 +0100

    test

--- src/test/kotlin/com/github/kpavlov/kappuccino/extensions/LocalDateExtensionsKtTest.kt
+++ src/test/kotlin/com/github/kpavlov/kappuccino/extensions/LocalDateExtensionsKtTest.kt
@@ -2,19 +2,21 @@ package com.github.kpavlov.kappuccino.extensions
 
 import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 internal class LocalDateExtensionsKtTest {
+
     @Test
     fun `should convert to OffsetDateTime`() {
         val src = java.time.LocalDateTime.now()
         val result = src.toOffsetDateTime()
         result?.toInstant() shouldBe src.toInstant(ZoneOffset.UTC)
     }
-//
-//    @Test
-//    fun `should convert null`() {
-//        assertNull(actual = null.toOffsetDateTime())
-//        (null.toOffsetDateTime() sh).isNull()
-//    }
+
+    @Test
+    fun `should convert null`() {
+        val receiver: LocalDateTime? = null
+        receiver.toOffsetDateTime() shouldBe null
+    }
 }
