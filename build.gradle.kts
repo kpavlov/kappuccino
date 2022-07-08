580c9f5449900bac555cd57e1dbbf61c9ee8ae87
Konstantin Pavlov <{ID}+{username}@users.noreply.github.com>
2022-07-08 10:43:07 +0300

    junit5 and kotest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     kotlin("jvm") version "1.7.0"
-    `java-library`
 }
 
 group = "com.github.kpavlov"
@@ -18,11 +18,24 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     api(kotlin("stdlib-jdk8"))
     // Test dependencies
+    testImplementation(platform("org.junit:junit-bom:5.7.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+
+    testImplementation("io.kotest:kotest-framework-api:$kotestVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+
 }
 
-tasks.withType<Test>().configureEach {
+tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events = setOf(
+            TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED
+        )
+    }
 }
 
 tasks.withType<KotlinCompile> {
